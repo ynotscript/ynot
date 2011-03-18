011113f2e9989768723a8bd06b3967c46e8e5ce9
equesada <equesada@ynotscript.com>
2011-03-18 18:28:13 +0100

    [FEATURE] add test

--- ynot-impl/src/test/java/ynot/impl/test/parser/request/tinyparser/MethodNameWithVariablesAndParametersTest.java
+++ ynot-impl/src/test/java/ynot/impl/test/parser/request/tinyparser/MethodNameWithVariablesAndParametersTest.java
@@ -12,50 +12,58 @@ import ynot.core.entity.Request;
 import ynot.core.exception.parser.UnparsableRequestException;
 import ynot.impl.parser.request.tinyparser.MethodNameWithVariablesAndParameters;
 
-
 /**
- * Mini parser for method with parameters and returned variables.
- * Example: $a := toto("abc")
+ * Mini parser for method with parameters and returned variables. Example: $a :=
+ * toto("abc")
+ * 
  * @author equesada
  */
 public class MethodNameWithVariablesAndParametersTest {
 
-	/**
-	 * Number of parameters when tests.
-	 */
-	private static final int NB_PARAMS = 3;
-	
-	/**
-	 * Number of parameters when tests.
-	 */
-	private static final int NB_VARS = 2;
-	/**
-	 * The mini parser to test.
-	 */
-	private MethodNameWithVariablesAndParameters mnwvap;
+    /**
+     * Number of parameters when tests.
+     */
+    private static final int NB_PARAMS = 3;
 
-	/**
-	 * To set up the mini parser.
-	 * @throws Exception if something is wrong.
-	 */
-	@Before
-	public final void setUp() throws Exception {
-		mnwvap = new MethodNameWithVariablesAndParameters();
-	}
+    /**
+     * Number of parameters when tests.
+     */
+    private static final int NB_VARS = 2;
+    /**
+     * The mini parser to test.
+     */
+    private MethodNameWithVariablesAndParameters mnwvap;
 
-	/**
-	 * Test the parse method.
-	 * @throws UnparsableRequestException if something is wrong.
-	 */
-	@Test
-	public final void testParse() throws UnparsableRequestException {
-		List<Request> reqs = mnwvap
-		    .parse(" $d , $e := p2\\test ( \"azerty\" , 2 , {1,2,\"o\"}) ");
-		assertTrue(reqs.size() == 1);
-		assertEquals(reqs.get(0).getWordToUse(), "test");
-		assertEquals(reqs.get(0).getDefinitionProviderName(), "p2");
-		assertTrue(reqs.get(0).getGivenParameters().length == NB_PARAMS);
-		assertTrue(reqs.get(0).getVariableNames().length == NB_VARS);
-		assertEquals(reqs.get(0).getVariableNames()[0], "d");
-	}
+    /**
+     * To set up the mini parser.
+     * 
+     * @throws Exception
+     *             if something is wrong.
+     */
+    @Before
+    public final void setUp() throws Exception {
+        mnwvap = new MethodNameWithVariablesAndParameters();
+    }
+
+    /**
+     * Test the parse method.
+     * 
+     * @throws UnparsableRequestException
+     *             if something is wrong.
+     */
+    @Test
+    public final void testParse() throws UnparsableRequestException {
+        List<Request> reqs = mnwvap
+                .parse(" $d , $e := p2\\test ( \"azerty\" , 2 , {1,2,\"o\"}) ");
+        assertTrue(reqs.size() == 1);
+        assertEquals(reqs.get(0).getWordToUse(), "test");
+        assertEquals(reqs.get(0).getDefinitionProviderName(), "p2");
+        assertTrue(reqs.get(0).getGivenParameters().length == NB_PARAMS);
+        assertTrue(reqs.get(0).getVariableNames().length == NB_VARS);
+        assertEquals(reqs.get(0).getVariableNames()[0], "d");
+
+        List<Request> reqs2 = mnwvap
+                .parse("$method := new(\"GetMethod\",{\"http://www.google.com/\"})");
+        assertTrue(reqs2.size() == 1);
+    }
 }
