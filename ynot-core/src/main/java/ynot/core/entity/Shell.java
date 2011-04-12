a672277d9fd6f39e468fb3327b7a4170c6d375d8
eric quesada <equesada@ynotscript.com>
2011-04-12 23:36:52 +0200

    [FEATURE] add the public licence at the beginning of the file [CHANGE] private to public for a method

--- ynot-core/src/main/java/ynot/core/entity/Shell.java
+++ ynot-core/src/main/java/ynot/core/entity/Shell.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2011 Eric Quesada
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package ynot.core.entity;
 
 import java.lang.reflect.InvocationTargetException;
@@ -42,7 +58,7 @@ public class Shell implements Cloneable {
 	 * @throws UnprovidableCommandException
 	 *             if commands aren't supplied
 	 */
-	private void loadAllCommands() throws UnprovidableCommandException {
+	public final void loadAllCommands() throws UnprovidableCommandException {
 		int step = 1;
 		while (commandProvider.hasNext()) {
 			commands.put(step++, commandProvider.getNext());
@@ -66,7 +82,7 @@ public class Shell implements Cloneable {
 	public final void run() throws UnprovidableCommandException,
 			UnprovidableResourceException, CloneNotSupportedException,
 			IllegalAccessException, InvocationTargetException {
-		if (!lazyLoading) {
+		if (!lazyLoading && commands.isEmpty()) {
 			loadAllCommands();
 		}
 		while (hasStep()) {
